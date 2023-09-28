d615b7be0d7da862625a7ad1ece94e37993d64b2
AnneEnglish <>
2023-09-28 16:24:40 +0000

    updated page title and added favicon

--- assets/js/script.js
+++ assets/js/script.js
@@ -96,6 +96,7 @@ let askQuestions = [
 let previousButton = document.getElementById('previous');
 let nextButton = document.getElementById('next');
 let submitButton = document.getElementById('submit');
+let restartButton = document.getElementById('restart');
 let userScore = document.getElementById('score');
 let questionText = document.getElementById('question-text');
 let optionsArea = document.getElementById('opt');
@@ -104,11 +105,21 @@ let optionsArea = document.getElementById('opt');
 let currentQuestion = 0;
 let score = 0;
 
+/*!BUG FIX 1! Flag variable to track if the quiz has been submitted already or 
+not*/
+let quizSubmitted = false;
+
+/* !BUG FIX 2! Array to keep track of whether each question was answered 
+correctly*/
+let questionAnsweredCorrectly = Array(askQuestions.length).fill(false);
+
 // Function to load the questions
 function loadQuiz() {
     questionText.innerHTML = askQuestions[currentQuestion].question;
     optionsArea.innerHTML = '';
 
+    /* For loop to iterate over the options while creating HTML elements
+    for each option and also appending them to the optionsArea element */
     for (let i = 0; i < askQuestions[currentQuestion].answer.length; i++) {
         let divChoices = document.createElement('div');
         divChoices.className = 'div-choices';
@@ -133,25 +144,42 @@ function loadQuiz() {
     }
 }
 
+/* Display user's score after completing the quiz, update userScore text with 
+score*/
 function loadScore() {
-    userScore.textContent =
-        `Well done on completing this quiz! Your score is ${score} out of 
-        ${askQuestions.length}!`;
+    userScore.textContent =`Well done on completing this quiz! Your score is 
+    ${score} out of ${askQuestions.length}!`;
 }
 
-function previousQuestion() {
-    if (currentQuestion > 0) {
-        currentQuestion--;
-        loadQuiz();
+function updateScore() {
+    score = 0;
+    for (let i = 0; i < askQuestions.length; i++) {
+        if (questionAnsweredCorrectly[i]) {
+            score++;
+        }
     }
 }
 
+
+function previousQuestion() {
+    /* !BUG FIX 2! Check and see if the current questions was answered correctly
+    before if it was decrement the score*/
+    if (currentQuestion > 0) {
+        if (questionAnsweredCorrectly[currentQuestion]) {
+            score--;
+            questionAnsweredCorrectly[currentQuestion] = false;
+        }
+        currentQuestion--;
+        loadQuiz();
+        updateScore();
+    }
+}
+
+// Increment currentQuestions and load next question
 function displayNextQuestion() {
     if (currentQuestion < askQuestions.length - 1) {
         currentQuestion++;
         loadQuiz();
-    } else {
-        loadScore();
     }
 }
 
@@ -162,12 +190,45 @@ function nextQuestion() {
     displayNextQuestion();
 }
 
+/* Check if the answer is correct and update the score accordingly.
+!BUG FIX 2! Parts of the function below were edited to help fix the second bug*/
 function handleAnswer(selectedAnswer) {
     if (askQuestions[currentQuestion].answer[selectedAnswer].isCorrect) {
-        score++;
+        if (!questionAnsweredCorrectly[currentQuestion]) {
+            score++;
+            questionAnsweredCorrectly[currentQuestion] = true;
+        }
+    } else {
+        questionAnsweredCorrectly[currentQuestion] = false;
     }
 }
 
+// Reset quiz to initial state, including current question and score
+function restartQuiz() {
+    // Reset current question and score to zero
+    currentQuestion = 0;
+    score = 0;
+    loadQuiz();
+
+    // Clear the user score text
+    userScore.textContent = '';
+
+    // Enable the submit button
+    submitButton.disabled = false;
+
+    // Reset the flag for quizSubmitted
+    quizSubmitted = false;
+
+    // !BUG FIX 3! Making the next and previous buttons visible again
+    previousButton.style.display = 'inline-block';
+    nextButton.style.display = 'inline-block';
+
+    // Recalculate the score
+    updateScore();
+}
+
+/* Function called when user clicks submit. Check if all questions have been 
+answered and calculate final score*/
 function checkAnswers() {
     let selectedRadio = document.querySelector('input[name="answer"]:checked');
     
@@ -175,9 +236,22 @@ function checkAnswers() {
         let selectedAnswer = parseInt(selectedRadio.value);
     
         if (askQuestions[currentQuestion].answer[selectedAnswer].isCorrect) {
-            score++;
+            if (!questionAnsweredCorrectly[currentQuestion]) {
+                score++
+                questionAnsweredCorrectly[currentQuestion] = true;
+            }
+        } else {
+            questionAnsweredCorrectly[currentQuestion] = false;
         } 
-        nextQuestion();
+
+        loadScore();
+
+        // !BUG FIX 1! Disbale submit button once quiz has been submitted
+        submitButton.disabled = true;
+        quizSubmitted = true;
+        // Hide buttons once quiz has been submitted
+        previousButton.style.display = 'none';
+        nextButton.style.display = 'none';
     } else {
         alert('Please answer all questions first');
     }
@@ -185,8 +259,15 @@ function checkAnswers() {
 
 // Onclick events for buttons
 previousButton.addEventListener('click', previousQuestion);
-submitButton.addEventListener('click', checkAnswers);
+/* !BUG FIX 1! Add function to submitButton event listener that stops it from adding 
+the score multiple times if clicked repeatedly at end of quiz*/
+submitButton.addEventListener('click', function () {
+    if (!quizSubmitted) {
+        checkAnswers();
+    }
+});
 nextButton.addEventListener('click', nextQuestion);
+restartButton.addEventListener('click', restartQuiz)
 
 // Load the first question when the page loads
 loadQuiz();
